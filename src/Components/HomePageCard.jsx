8adddbf1ebf1616c07a8d640af9097406eb81a5f
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2025-10-19 18:31:58 +0530

    small bug fixd

--- src/Components/HomePageCard.jsx
+++ src/Components/HomePageCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import WishlistButton from "./WishlistButton";
-
 const HomePageCard = ({ id, img, name, price, dis }) => {
   // Forward to product Detail page with ID
   const navigate = useNavigate();
@@ -9,12 +8,16 @@ const HomePageCard = ({ id, img, name, price, dis }) => {
     navigate(`/productdetail/${id}`);
   };
   return (
-    <div className="rounded-md p-2 cursor-pointer">
-      <div className="bg-blue-400/30 rounded-xl relative">
-        <div className="absolute p-2 right-0 text-xl">
+    <div className="rounded-md p-2 cursor-pointer ">
+      <div className="bg-blue-400/30 rounded-xl ">
+        <div className="flex justify-end text-2xl">
           <WishlistButton productId={id} />
         </div>
-        <img src={img || "/ImageNotA.jpg"} alt={name} onClick={() => Product(id)} />
+        <img
+          src={img || "/ImageNotA.jpg"}
+          alt={name}
+          onClick={() => Product(id)}
+        />
       </div>
       <p className="text-xl ml-2" onClick={() => Product(id)}>
         {name}
